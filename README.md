2b8dfb4d5c20c10f1173063306a89e2ab4e2c57d
frncsdrk <carlsmoh@gmail.com>
2018-10-08 23:17:53 +0200

    Add script
    update readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# supa
+supa
+===
+
 bash tool to update linux servers, also means soup in greek
+
+## usage
+
+```sh
+./supa.sh <user>@<host> [-u] [-r]
+```
---
+++ supa.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+usage() { echo "Usage: ./supa.sh <user>@<host> [-u] [-r]"; }
+
+while getopts "ho:ru" option
+do
+  case "${option}"
+  in
+    h)
+      usage
+      exit 0
+      ;;
+    o)
+      OPERATOR=${OPTARG}
+      ;;
+    r)
+      REBOOT=${OPTIND}
+      ;;
+    u)
+      UPGRADE=${OPTIND}
+      ;;
+  esac
+done
+
+SCRIPT=$'sudo apt update'
+SCRIPT+=$'\napt list --upgradeable'
+if [ ! -z "$UPGRADE" ]; then
+  SCRIPT+=$'\nsudo apt upgrade -y'
+fi
+if [ ! -z "$REBOOT" ]; then
+  SCRIPT+=$'\nif [ -f "/var/run/reboot-required" ]; then'
+  SCRIPT+=$'\n  sudo /sbin/reboot now'
+  SCRIPT+=$'\nfi'
+fi
+
+ssh "$OPERATOR" "$SCRIPT"
