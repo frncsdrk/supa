bc8d6c0c3648a67027ebf4fedd96d920c5716510
frncsdrk <carlsmoh@gmail.com>
2018-10-19 17:23:58 +0200

    Replace package option by adding the package to upgrade to upgrade option
    update usage
    update readme

--- README.md
+++ README.md
@@ -11,13 +11,12 @@ supa
 supa
 
 Usage:
-  ./supa.sh -o <user>@<host> [-h|--help] [-u|--upgrade] [-p|--package] [-a|--autoremove] [-r|--reboot] [-v|--version]
+  ./supa.sh -o <user>@<host> [-h|--help] [-u|--upgrade [package]] [-a|--autoremove] [-r|--reboot] [-v|--version]
 
 Options:
   -a|--autoremove                                  autoremove
   -h|--help                                        help
   -o|--operator                                    operator
-  -p|--package                                     package
   -r|--reboot                                      reboot
   -u|--upgrade                                     upgrade
   -v|--version                                     version
@@ -26,7 +25,7 @@ Examples:
   ./supa.sh -h                                     display this message
   ./supa.sh -o you@remote-host                     run apt update and apt list --upgradeable
   ./supa.sh -o you@remote-host -u                  same as the former but with the addition of upgrading all packages
-  ./supa.sh -o you@remote-host -u -p [package]     same as the former but with the addition of upgrading one single package
+  ./supa.sh -o you@remote-host -u [package]        same as the former but with the addition of upgrading one single package
   ./supa.sh -o you@remote-host -u -r               same as the former but with the addition of allowing reboot if necessary
   ./supa.sh -o you@remote-host -u -a -r            same as the former but with the addition of autoremoving of obsolete packages
 
--- supa.sh
+++ supa.sh
@@ -7,13 +7,12 @@ usage() {
 supa
 
 Usage:
-  ./supa.sh -o <user>@<host> [-h|--help] [-u|--upgrade] [-p|--package] [-a|--autoremove] [-r|--reboot] [-v|--version]
+  ./supa.sh -o <user>@<host> [-h|--help] [-u|--upgrade [package]] [-a|--autoremove] [-r|--reboot] [-v|--version]
 
 Options:
   -a|--autoremove                                  autoremove
   -h|--help                                        help
   -o|--operator                                    operator
-  -p|--package                                     package
   -r|--reboot                                      reboot
   -u|--upgrade                                     upgrade
   -v|--version                                     version
@@ -22,7 +21,7 @@ Examples:
   ./supa.sh -h                                     display this message
   ./supa.sh -o you@remote-host                     run apt update and apt list --upgradeable
   ./supa.sh -o you@remote-host -u                  same as the former but with the addition of upgrading all packages
-  ./supa.sh -o you@remote-host -u -p [package]     same as the former but with the addition of upgrading one single package
+  ./supa.sh -o you@remote-host -u [package]        same as the former but with the addition of upgrading one single package
   ./supa.sh -o you@remote-host -u -r               same as the former but with the addition of allowing reboot if necessary
   ./supa.sh -o you@remote-host -u -a -r            same as the former but with the addition of autoremoving of obsolete packages
 
@@ -48,17 +47,16 @@ do
       shift
       shift
       ;;
-    -p|--package)
-      UPGRADE_PACKAGE="$2"
-      shift
-      shift
-      ;;
     -r|--reboot)
       REBOOT=1
       shift
       ;;
     -u|--upgrade)
       UPGRADE=1
+      if [[ $2 != "-"* ]]; then
+        UPGRADE_PACKAGE="$2"
+      fi
+      shift
       shift
       ;;
     -v|--version)
@@ -84,7 +82,7 @@ if [ ! -z "$UPGRADE" ]; then
   fi
 fi
 if [ ! -z "$AUTOREMOVE" ]; then
-  SCRIPT+=$'\nsudo apt autoremove'
+  SCRIPT+=$'\nsudo apt -y autoremove'
 fi
 if [ ! -z "$REBOOT" ]; then
   SCRIPT+=$'\nif [ -f "/var/run/reboot-required" ]; then'
