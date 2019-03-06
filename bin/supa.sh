6b410d15404c6537d401f6ad3ec226b005bb6b1c
frncsdrk <carlsmoh@gmail.com>
2019-03-06 17:02:12 +0100

    Add setup script
    add manpage
    update usage

--- bin/supa.sh
+++ bin/supa.sh
@@ -8,35 +8,75 @@ usage() {
   cat << EOF
 supa
 
-Usage:
+Synopsis:
   ./supa.sh <user>@<host> [-h|--help] [-v|--version] [-l|--list] [--list-off]
   [-i|--identity <identity file>] [-u|--upgrade <package>] [-a|--autoremove] [-m|--machines]
   [-b|--reboot-required] [-r|--reboot] [-d|--debug]
 
 Options:
-  -a|--autoremove                                  autoremove
-  -b|--reboot-required                             reboot required
-  -d|--debug                                       enable debug mode
-  -h|--help                                        help
-  -i|--identity                                    identity
-  -l|--list                                        list
-  --list-off                                       list off, only update
-  -m|--machines                                    use given machines file
-  -r|--reboot                                      reboot
-  -u|--upgrade                                     upgrade
-  -v|--version                                     version
+  -a|--autoremove
+          autoremove
+
+  -b|--reboot-required
+          reboot required
+
+  -d|--debug
+          enable debug mode
+
+  -h|--help
+          show this message
+
+  -i|--identity
+          identity
+
+  -l|--list
+          list
+
+  --list-off
+          list off, only update
+
+  -m|--machines
+          use given machines file
+
+  -r|--reboot
+          reboot
+
+  -u|--upgrade
+          upgrade
+
+  -v|--version
+          version
 
 Examples:
-  ./supa.sh -v                                     display version
-  ./supa.sh -h                                     display this message
-  ./supa.sh you@remote-host -b                     is machine reboot required
-  ./supa.sh you@remote-host -b -l                  is machine reboot required, but list upgradeable packages as well
-  ./supa.sh -m production -b -l                    same as previous but use machines file
-  ./supa.sh you@remote-host                        run apt update and apt list --upgradeable
-  ./supa.sh you@remote-host -u                     same as the former but with the addition of upgrading all packages
-  ./supa.sh you@remote-host -u <package>           same as the former but with the addition of upgrading one single package
-  ./supa.sh you@remote-host -u -r                  same as the former but with the addition of allowing reboot if necessary
-  ./supa.sh you@remote-host -u -a -r               same as the former but with the addition of autoremoving of obsolete packages
+  supa.sh -v
+          display version
+
+  supa.sh -h
+          display this message
+
+  supa.sh you@remote-host -b
+          is machine reboot required
+
+  supa.sh you@remote-host -b -l
+          is machine reboot required, but list upgradeable packages as well
+
+  supa.sh -m production -b -l
+          same as previous but use machines file
+
+  supa.sh you@remote-host
+          run apt update and apt list --upgradeable
+
+  supa.sh you@remote-host -u
+          same as the former but with the addition of upgrading all packages
+
+  supa.sh you@remote-host -u <package>
+          same as the former but with the addition of upgrading one single package
+
+  supa.sh you@remote-host -u -r
+          same as the former but with the addition of allowing reboot if necessary
+
+  supa.sh you@remote-host -u -a -r
+          same as the former but with the addition of autoremoving of obsolete packages
 
 EOF
 }
@@ -163,7 +203,7 @@ main() {
   if [ ! -z "$DEBUG" ]; then
     printf '%s\n' "$SCRIPT"
   fi
-  
+
   for machine in $machines
   do
     printf '\n%s\n' '###'
---
+++ setup.sh
@@ -0,0 +1,134 @@
+#!/usr/bin/env bash
+
+INSTALLABLE_NAME="supa.sh"
+
+usage() {
+  cat << EOF
+setup
+
+Synopsis:
+  ./setup <i|install|u|uninstall|upgrade>
+
+Commands:
+  i|install
+          install supa.sh and create its manpage
+  u|uninstall
+          uninstall supa.sh and remove its manpage
+  upgrade
+          update manpage
+EOF
+}
+
+# ensure linux
+check_os() {
+  if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "linux-musl" ]] ; then
+    readonly _dir=$(dirname "$(readlink -f "$0" || echo "$(echo "$0" | sed -e 's,\\,/,g')")")
+  else
+    printf "Unsupported system version.\\n"
+    exit 1
+  fi
+}
+
+get_args() {
+  if [[ -z $1 ]]; then
+    usage
+  fi
+
+  local POSITIONAL=()
+  while [[ $# -gt 0 ]]
+  do
+    local key=$1
+
+    case $key in
+      -d|--debug)
+        DEBUG=1
+        shift
+        ;;
+      -h|--help)
+        usage
+        exit 0
+        ;;
+      i|install)
+        install
+        exit 0
+        ;;
+      u|uninstall)
+        uninstall
+        exit 0
+        ;;
+      upgrade)
+        upgrade
+        exit 0
+        ;;
+      -v|--version)
+        printf '%s\n' "$VERSION"
+        exit 0
+        ;;
+      *)
+        # get operator
+        if [[ $1 =~ ^.+@.+$ ]]; then
+          OPERATOR="$1"
+        fi
+        POSITIONAL+=("$1")
+        shift
+        ;;
+    esac
+  done
+  set -- "${POSITIONAL[@]}"
+}
+
+install_manpage() {
+  printf "%s\\n" "Create man page to /usr/local/man/man8"
+
+  if [[ -e "${_dir}/static/man8/${INSTALLABLE_NAME}.8" ]] ; then
+    if [[ ! -e "/usr/local/man/man8/${INSTALLABLE_NAME}.8.gz" ]] ; then
+      mkdir -p /usr/local/man/man8
+      cp "${_dir}/static/man8/${INSTALLABLE_NAME}.8" /usr/local/man/man8
+      gzip /usr/local/man/man8/${INSTALLABLE_NAME}.8
+    fi
+  fi
+}
+
+install() {
+  printf "%s\\n" "Create symbolic link to /usr/local/bin"
+
+  if [[ -e "${_dir}/bin/${INSTALLABLE_NAME}" ]] ; then
+    if [[ ! -e "/usr/local/bin/${INSTALLABLE_NAME}" ]] ; then
+      ln -s "${_dir}/bin/${INSTALLABLE_NAME}" /usr/local/bin
+    fi
+  fi
+
+  install_manpage
+}
+
+uninstall_manpage() {
+  printf "%s\\n" "Remove man page from /usr/local/man/man8"
+
+  if [[ -e "/usr/local/man/man8/${INSTALLABLE_NAME}.8.gz" ]] ; then
+    rm /usr/local/man/man8/${INSTALLABLE_NAME}.8.gz
+  fi
+}
+
+uninstall() {
+  printf "%s\\n" "Remove symbolic link from /usr/local/bin"
+
+  if [[ -L "/usr/local/bin/${INSTALLABLE_NAME}" ]] ; then
+    unlink /usr/local/bin/${INSTALLABLE_NAME}
+  fi
+
+  uninstall_manpage
+}
+
+upgrade() {
+  printf "%s\\n" "Replace man page from /usr/local/man/man8"
+
+  uninstall_manpage
+  install_manpage
+}
+
+main() {
+  check_os
+  get_args "$@"
+}
+
+main "$@"
