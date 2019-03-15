18a74a3c81cdbee63323586ab9a991f8feb8ca17
frncsdrk <frncsdrk@protonmail.com>
2019-03-15 21:08:48 +0100

    Fix formatting

--- lib/installer.sh
+++ lib/installer.sh
@@ -5,7 +5,7 @@
 uninstall_manpage() {
   printf '%s\n' "Remove man page from /usr/local/man/man8"
 
-  if [[ -e "/usr/local/man/man8/${INSTALLABLE_NAME}.8.gz" ]] ; then
+  if [[ -e "/usr/local/man/man8/${INSTALLABLE_NAME}.8.gz" ]]; then
     rm /usr/local/man/man8/${INSTALLABLE_NAME}.8.gz
   fi
 }
@@ -13,13 +13,13 @@ uninstall_manpage() {
 uninstall() {
   printf '%s\n' "Remove installation"
   
-  if [[ -d "${INSTALL_DIRECTORY_PATH}/${INSTALLABLE_NAME}" ]] ; then
+  if [[ -d "${INSTALL_DIRECTORY_PATH}/${INSTALLABLE_NAME}" ]]; then
     rm -r "${INSTALL_DIRECTORY_PATH}/${INSTALLABLE_NAME}"
   fi
   
   printf '%s\n' "Remove symbolic link from /usr/local/bin"
 
-  if [[ -L "/usr/local/bin/${INSTALLABLE_NAME}" ]] ; then
+  if [[ -L "/usr/local/bin/${INSTALLABLE_NAME}" ]]; then
     unlink /usr/local/bin/${INSTALLABLE_NAME}
   fi
 
