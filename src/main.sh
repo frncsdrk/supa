66eb78479dcf6a4990377f72cb8fe15f8f72612a
frncsdrk <frncsdrk@protonmail.com>
2021-06-03 11:57:45 +0200

    Fix several shellcheck warnings

--- src/main.sh
+++ src/main.sh
@@ -7,33 +7,33 @@ build_script() {
   # ensure non interactive mode
   SCRIPT+="export DEBIAN_FRONTEND=noninteractive"
 
-  if [ -z "${REBOOT}" ] || [ ! -z "${LIST}" ]; then
-    if [ -z "${REBOOT_REQUIRED}" ] || [ ! -z "${LIST}" ]; then
+  if [ -z "${REBOOT}" ] || [ -n "${LIST}" ]; then
+    if [ -z "${REBOOT_REQUIRED}" ] || [ -n "${LIST}" ]; then
       SCRIPT+=$'\nsudo apt update'
       if [ -z "${LIST_OFF}" ]; then
         SCRIPT+=$'\napt list --upgradeable'
       fi
     fi
   fi
-  if [ ! -z "${UPGRADE}" ]; then
-    if [ ! -z "${UPGRADE_PACKAGE}" ]; then
+  if [ -n "${UPGRADE}" ]; then
+    if [ -n "${UPGRADE_PACKAGE}" ]; then
       SCRIPT+=$'\nsudo apt install -q --only-upgrade '
       SCRIPT+="${UPGRADE_PACKAGE}"
     else
       SCRIPT+=$'\nsudo apt -q upgrade -y'
     fi
   fi
-  if [ ! -z "${AUTOREMOVE}" ]; then
+  if [ -n "${AUTOREMOVE}" ]; then
     SCRIPT+=$'\nsudo apt autoremove -y'
   fi
-  if [ ! -z "${REBOOT}" ] || [ ! -z "${REBOOT_REQUIRED}" ]; then
+  if [ -n "${REBOOT}" ] || [ -n "${REBOOT_REQUIRED}" ]; then
     SCRIPT+=$'\nif [ -f "/var/run/reboot-required" ]; then'
-    if [ ! -z "${REBOOT_REQUIRED}" ]; then
+    if [ -n "${REBOOT_REQUIRED}" ]; then
       SCRIPT+=$'\n  printf \'\n%s\n\' "###########################"'
       SCRIPT+=$'\n  printf \'%s\n\'   "# machine reboot required #"'
       SCRIPT+=$'\n  printf \'%s\n\n\'   "###########################"'
     fi
-    if [ ! -z "${REBOOT}" ]; then
+    if [ -n "${REBOOT}" ]; then
       SCRIPT+=$'\n  printf \'%s\n\n\'   "rebooting..."'
       SCRIPT+=$'\n  sudo shutdown -r'
     fi
@@ -46,7 +46,7 @@ main() {
   build_script
   generate_machines_list
 
-  if [ ! -z "${DEBUG}" ]; then
+  if [ -n "${DEBUG}" ]; then
     printf '%s\n' "${SCRIPT}"
   fi
 
@@ -54,13 +54,13 @@ main() {
   do
     printf '\n%s\n' '###'
     printf '%s\n' "# ${machine}"
-    if [ ! -z "${VERBOSE}" ]; then
+    if [ -n "${VERBOSE}" ]; then
       printf '%s\n' '##'
       build_cmd "$@"
       printf '%s\n' "# cmd: ${executed_cmd}"
     fi
     printf '%s\n\n' '###'
-    if [ ! -z "${IDENTITY}" ]; then
+    if [ -n "${IDENTITY}" ]; then
       ssh -i "${IDENTITY}" -o IdentitiesOnly=yes "${machine}" "${SCRIPT}"
     else
       ssh "${machine}" "${SCRIPT}"
